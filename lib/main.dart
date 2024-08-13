b168e7c739e2c67b74ce20193431601610c15039
Dhipin03 <Dhipinmkumar2000@gmail.com>
2024-08-13 11:00:34 +0530

    Added List<Map>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,108 +9,123 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    DateTime now = DateTime.now();
-    DateTime indianTime = now.toUtc().add(Duration(hours: 5, minutes: 30));
-    List name = [
-      'Test1 ${indianTime.hour}h',
-      'Test2 ${indianTime.hour + 2}h',
-      'Test3 ${indianTime.hour + 3}h',
+    List<Map> datalist1 = [
+      {
+        "name": "person1",
+        "dp":
+            "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?t=st=1723521475~exp=1723525075~hmac=58b01d0b841d9bbc691dfe21ef7660dab6f9e89bc6a2ad4e2ef174ea68271373&w=1380",
+        "bg":
+            "https://images.pexels.com/photos/3094799/pexels-photo-3094799.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      },
+      {
+        "name": "person2",
+        "dp":
+            "https://img.freepik.com/free-photo/beautiful-woman-with-curly-hair_23-2148778875.jpg?w=1380&t=st=1723524037~exp=1723527637~hmac=bf20b54219cb75d4c43b330065dd10d6357f3e1dd26ae03513be04d77977eec4",
+        "bg":
+            "https://images.pexels.com/photos/4380127/pexels-photo-4380127.jpeg?auto=compress&cs=tinysrgb&w=800"
+      },
+      {
+        "name": "person3",
+        "dp":
+            "https://img.freepik.com/free-photo/handsome-young-man-smiling-outdoors_23-2148217344.jpg?w=1380&t=st=1723524085~exp=1723527685~hmac=5394fd8d65a9a9c9cb9af38c7aa9b3e7b8d19a91f90d0cbcd9f08742b7418e7d",
+        "bg":
+            "https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&w=800"
+      },
+      {
+        "name": "person4",
+        "dp":
+            "https://img.freepik.com/free-photo/portrait-beautiful-asian-woman-wearing-white-shirt_53876-102547.jpg?w=1380&t=st=1723524121~exp=1723527721~hmac=0c2283a55d61b17d8e7a6d11706be49053f22fb129d367aab93f1a64a2fcbf85",
+        "bg":
+            "https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&cs=tinysrgb&w=800"
+      },
+      {
+        "name": "person5",
+        "dp":
+            "https://img.freepik.com/free-photo/portrait-smiling-young-man-with-glasses_1262-20852.jpg?w=1380&t=st=1723524157~exp=1723527757~hmac=745a30901dfb133bbd6a2f2f961eaaab0e4c99f62f209c7bff64d5c03017f85e",
+        "bg":
+            "https://images.pexels.com/photos/2063244/pexels-photo-2063244.jpeg?auto=compress&cs=tinysrgb&w=800"
+      },
+      {
+        "name": "person6",
+        "dp":
+            "https://img.freepik.com/free-photo/confident-businesswoman-posing-outdoor_23-2148908569.jpg?w=1380&t=st=1723524210~exp=1723527810~hmac=fcccb9a550bb30a65c6d86bcb84f1d69f3a5e2920b55a768643c37df95ef28ed",
+        "bg":
+            "https://images.pexels.com/photos/3255234/pexels-photo-3255234.jpeg?auto=compress&cs=tinysrgb&w=800"
+      }
     ];
-    List avat = [
-      'https://images.unsplash.com/photo-1518611540400-6b85a0704342?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      'https://images.unsplash.com/photo-1500832333538-837287aad2b6?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      'https://images.unsplash.com/photo-1521146764736-56c929d59c83?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    ];
-    List imagelist = [
-      'https://plus.unsplash.com/premium_photo-1663852296749-30454b4613e8?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      "https://images.unsplash.com/photo-1577121443425-97e029fccc2d?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "https://images.unsplash.com/photo-1580163661417-3606299aba72?q=80&w=2831&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    ];
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: PageView.builder(
-          itemCount: imagelist.length,
-          itemBuilder: (context, index) => Container(
-            child: Column(
-              children: [
-                SizedBox(height: 41),
-                Divider(
-                  indent: 4,
-                  endIndent: 4,
-                  thickness: 2,
-                ),
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(avat[index]),
-                  ),
-                  title: Text(
-                    '${name[index]}',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  trailing: Icon(
-                    Icons.more_vert,
-                    color: Colors.white,
-                  ),
-                ),
-                Spacer(),
-                Container(
-                  width: double.infinity,
-                  height: 90,
-                  color: Colors.black,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 32, left: 12),
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                                label: Text(
-                                  "Send message",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(32)),
-                                    borderSide: BorderSide(
-                                      color: Colors.white,
-                                    ))),
-                          ),
-                        ),
-                      ),
-                      Icon(
-                        Icons.favorite_outline,
-                        color: Colors.white,
-                        size: 32,
-                      ),
-                      SizedBox(width: 23),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.send,
-                          color: Colors.white,
-                          size: 32,
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: NetworkImage(
-                  imagelist[index],
-                ),
-              ),
+          body: SafeArea(
+              child: PageView.builder(
+        itemCount: datalist1.length,
+        itemBuilder: (context, index) => Container(
+          padding: EdgeInsets.symmetric(horizontal: 17, vertical: 17),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(datalist1[index]["bg"]),
             ),
           ),
+          child: Column(
+            children: [
+              Divider(
+                thickness: 2,
+                color: Colors.white,
+              ),
+              ListTile(
+                subtitle: Text(
+                  "User name",
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: Icon(
+                  Icons.more_vert,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  datalist1[index]["name"],
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(datalist1[index]["dp"]),
+                ),
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(22),
+                          border: Border.all(color: Colors.white, width: 1)),
+                      child: Text(
+                        "Send message",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Icon(
+                    Icons.favorite_outline_outlined,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  )
+                ],
+              )
+            ],
+          ),
         ),
-      ),
+      ))),
     );
   }
 }
